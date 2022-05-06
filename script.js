ab53309b4840ff8a74c87fdabc4a54399b94f49b
alindaByamukama <alindabyamukama@gmail.com>
2022-05-06 16:18:13 +0300

    JS: created a function to check results, increase score, update resultText

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let playerScoreNumber = 0
+let computerScoreNumber = 0
 let computerChoice = ''
 
 // Reset all 'selected' icons
@@ -79,16 +81,39 @@ function displayComputerChoice() {
   }
 }
 
+// Check result, increase scores, update resultText
+function updateScore(playerChoice) {
+  console.log(playerChoice, computerChoice)
+
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie."
+  } else {
+    const choice = choices[playerChoice]
+    console.log(choice.defeats.indexOf(computerChoice))
+
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+       resultText.textContent = "You won!"
+       playerScoreNumber++
+       playerScoreEl.textContent = playerScoreNumber
+    } else {
+      resultText.textContent = "You lost."
+      computerScoreNumber++
+      computerScoreEl.textContent = computerScoreNumber
+    }
+  }
+}
+
 // Call functions to process turn
-function checkResult() {
+function checkResult(playerChoice) {
   resetSelected()
   computerRandomChoice()
   displayComputerChoice()
+  updateScore(playerChoice)
 }
 
 // Passing player selection value, styling icons
 function select(playerChoice) {
-  checkResult()
+  checkResult(playerChoice)
   // Add 'selected' styling & playerChoice
   switch (playerChoice) {
     case 'rock':
